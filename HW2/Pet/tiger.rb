3c604bc30d70b40b47c15299d791327c116dfdd2
Andrew_Lux <nefesh.samael@gmail.com>
2020-11-17 17:35:53 +0200

    refactoring

--- HW2/Pet/tiger.rb
+++ HW2/Pet/tiger.rb
@@ -1,17 +1,17 @@
 require_relative 'pet'
 class Tiger < Pet
-	REACTIONS = [
-		['Тигренок начинает вылизывать свою шерсть.'],
-		['Вы наблюдаете, как тигренок затаился и вслушивается в звуки.'], 
-		['Вы видите, как тигренок точит когти об кору дерева.']
-	].freeze
+    REACTIONS = [
+        ['Тигренок начинает вылизывать свою шерсть.'],
+        ['Вы наблюдаете, как тигренок затаился и вслушивается в звуки.'], 
+        ['Вы видите, как тигренок точит когти об кору дерева.']
+    ].freeze
 
-	def initialize(name)
-		super
-	end
+    def initialize(name)
+        super
+    end
 
-	def random
-		puts REACTIONS.sample.map
-		timePassed
-	end
+    def random
+        puts REACTIONS.sample.map
+        timePassed
+    end
 end
